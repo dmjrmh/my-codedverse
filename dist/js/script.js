ad95e4bee457304bbc501622be6617de770f6611
Dame <gban1mj27@gmail.com>
2025-02-21 09:12:47 +0700

    added some dark mode

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,6 +2,7 @@
 
 const hamburger = document.querySelector("#hamburger-menu")
 const navMenu = document.querySelector("#navbar-menu")
+const btnToTop = document.querySelector("#fab-top")
 
 hamburger.addEventListener('click', function(){
   hamburger.classList.toggle('hamburger-active')
@@ -15,7 +16,34 @@ window.onscroll = function (){
 
   if (window.scrollY > fixedNav) {
     header.classList.add('navbar-fixed')
+    btnToTop.classList.add('flex')
+    btnToTop.classList.remove('hidden')
   } else {
+    btnToTop.classList.add('hidden')
+    btnToTop.classList.remove('flex')
     header.classList.remove('navbar-fixed')
   }
 }
+
+// outside click on hamburger menu
+window.addEventListener('click', function (e) {
+  if(e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove('hamburger-active')
+    navMenu.classList.add('hidden')
+  }
+})
+
+// toggle darkmode
+const toggleDarkMode = document.querySelector('#darkmode-toggle')
+const html = document.querySelector('html')
+const logo = document.getElementById("logo-navbar");
+
+toggleDarkMode.addEventListener('click', function() {
+  if (toggleDarkMode.checked) {
+    html.classList.add('dark')
+    logo.src = "dist/img/navbar/lg-nvbr-white-rmbg.png";
+  } else {
+    html.classList.remove('dark')
+    logo.src = "dist/img/navbar/lg-nvbr-rmbg.png";
+  }
+})
